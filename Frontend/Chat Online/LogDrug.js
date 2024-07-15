c012e965f27026025b22f9cd653d416f6464b15b
hlbaot <baotoanit1505@gmail.com>
2024-07-15 18:27:44 +0700

    done sign up form

---
+++ Frontend/Chat Online/LogDrug.js
@@ -0,0 +1,11 @@
+const show = () => {
+    const signUp = document.querySelector("#sign_up");
+    signUp.style.display = "block";
+    document.querySelector(".layer").style.display = "block";
+  };
+  
+  const hide = () => {
+    const signUp = document.querySelector("#sign_up");
+    signUp.style.display = "none";
+    document.querySelector(".layer").style.display = "none";
+  };
